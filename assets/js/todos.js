43e7423e2b38b2a3fe607a08af984a2b5e44c3a8
Anuj Sachdeva <anujsachdeva100@gmail.com>
2018-03-09 16:54:51 +0530

    initialized git repository

---
+++ assets/js/todos.js
@@ -0,0 +1,29 @@
+
+
+
+$("ul").on("click", "li", function(){
+	 $(this).toggleClass("completed");
+  }); 
+
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	})
+	event.stopPropagation();
+});
+
+$("input[type = 'text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbing the text from the input
+		  var todoText = $(this).val();
+		  alert(todoText);
+		//clearing the input
+		  $(this).val("");
+		//adding the input to the list
+		 $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+	};
+});
+
+$(".fa-plus").click(function(){
+	$("input[type = 'text']").fadeToggle();
+})
